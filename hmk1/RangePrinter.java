373def952cf719e8fda506f885d5d4a6ed26e53f
ecenurdemirx <22COMP1017@isik.edu.tr>
2026-01-02 19:20:21 +0300

    Fix duplicated code after merge conflict

--- hmk1/RangePrinter.java
+++ hmk1/RangePrinter.java
@@ -14,27 +14,10 @@ public class RangePrinter {
         int end = Math.max(a, b) - 1;
 
         for (int i = start; i <= end; i++) {
-            System.out.println(i);
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
-import java.util.Scanner;
 
-public class RangePrinter {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        printBetween(a, b);
-        sc.close();
-    }
-
-    static void printBetween(int a, int b) {
-        int start = Math.min(a, b) + 1;
-        int end = Math.max(a, b) - 1;
-
-        for (int i = start; i <= end; i++) {
-            System.out.println(i);
-}
-}
-}
