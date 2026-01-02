bcbf6dbc7bc64aaa75716c7c3332ea9ca9c33b85
ecenurdemirx <22COMP1017@isik.edu.tr>
2026-01-02 19:11:03 +0300

    satisfying requirement change two

--- hmk1/RangePrinter.java
+++ hmk1/RangePrinter.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class RangePrinter {
@@ -13,28 +14,11 @@ public class RangePrinter {
         int start = Math.min(a, b) + 1;
         int end = Math.max(a, b) - 1;
 
-        for (int i = start; i <= end; i++) {
-            System.out.println(i);
-        }
+       
+
+for (int i = start; i <= end; i++) {
+    if (i % 2 != 0) {
+        System.out.println(i);
     }
-}
-import java.util.Scanner;
-
-public class RangePrinter {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        printBetween(a, b);
-        sc.close();
-    }
-
-    static void printBetween(int a, int b) {
-        int start = Math.min(a, b) + 1;
-        int end = Math.max(a, b) - 1;
-
-        for (int i = start; i <= end; i++) {
-            System.out.println(i);
-}
 }
 }
